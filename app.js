38a7c934010b90dcc7ec1c5d77d30eed9a534391
CamilaAguirre43 <camilaaguirre458@gmail.com>
2021-06-04 05:12:49 -0300

    personajes e historias

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ app.use(express.static('public'));
 
 app.listen(3030, () => console.log("Servidor corriendo por el puerto "+ port));
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
-app.get('/aang',(req,res)=> res.sendFile(path.join(__dirname,'views','aang.html')));
-app.get('/katara',(req,res)=> res.sendFile(path.join(__dirname,'views','katara.html')));
-app.get('/sokka',(req,res)=> res.sendFile(path.join(__dirname,'views','sokka.html')));
-app.get('/toph',(req,res)=> res.sendFile(path.join(__dirname,'views','toph.html')));
-app.get('/zuko',(req,res)=> res.sendFile(path.join(__dirname,'views','zuko.html')));
+app.get('/views/aang.html',(req,res)=> res.sendFile(path.join(__dirname,'views','aang.html')));
+app.get('/views/katara.html',(req,res)=> res.sendFile(path.join(__dirname,'views','katara.html')));
+app.get('/views/sokka.html',(req,res)=> res.sendFile(path.join(__dirname,'views','sokka.html')));
+app.get('/views/toph.html',(req,res)=> res.sendFile(path.join(__dirname,'views','toph.html')));
+app.get('/views/zuko.html',(req,res)=> res.sendFile(path.join(__dirname,'views','zuko.html')));
 
 
 
